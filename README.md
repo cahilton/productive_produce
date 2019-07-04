145231321412ba4ddb378e788b6694eb85acfea5
Charity Hilton <Charity.Hilton@gtri.gatech.edu>
2019-07-04 12:22:23 -0400

    client stub

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Productive Produce
 
 Simple demo app for Georgia Tech Summer 2019, CS 6795 (Intro to Cognitive Science).
+
+## Running Client
+`cd client`
+
+`yarn install`
+
+`yarn start`
---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './bell-pepper.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3>Productive Produce</h3>
+      </header>
+      <p className="App-body">
+
+          Hi!
+      </p>
+    </div>
+  );
+}
+
+export default App;
